9784bd109734332d3f177c421a5ccfc07f1107ce
alfredoaea <alfredo_alejandro@live.com.mx>
2021-11-25 00:22:45 -0600

    taller en funcion

--- cotizacionesEjercicio/inc/conexion.php
+++ cotizacionesEjercicio/inc/conexion.php
@@ -1,7 +1,7 @@
 ﻿<?php
 $db_host = "192.168.56.1";
-$db_user = "AlfredoAlejandro";
-$db_password = "Alejandro00";
+$db_user = "alfredo";
+$db_password = "12345";
 $db_name = "tallerbd";
 
 $con = new mysqli($db_host,$db_user,$db_password,$db_name);
